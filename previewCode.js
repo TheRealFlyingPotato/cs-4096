75a0ff0fd09b58533ae80f2fe110f35c1d8272c6
Michael Ward <mcw625@mst.edu>
2019-02-19 15:05:34 -0600

    adding categories

--- previewCode.js
+++ previewCode.js
@@ -14,19 +14,16 @@ $( document ).ready(function() {
   $(document).on('keypress', function(e) {
     if(e.which == 13 && document.activeElement == $("#cardname")[0])
     {
-      // if (document.activeElement == $("#cardname")[0])
-      // {
-        e.preventDefault(); // ignores form submission
-        transferToMaindeck();
-      // }
-
+      e.preventDefault(); // ignores form submission
+      transferToMaindeck();
     }
   })
 
-  // setInterval(checkPageFocus, 300);
-
   if (TESTING) {
-    $("#cardEntry").val("Cancel\nWhisper, Blood Liturgist *CMDR");
+    $("#cardEntry").val("Cancel*Control *Instant\nWhisper, Blood Liturgist *CMDR\nMystic Confluence *Control*Instant*Draw \nGenerous Stray");
+    $("#test").on('click', function(){
+      buildCardDataFromString($("#cardEntry").val());
+    });
   }
 
   console.log("ready!");
@@ -43,24 +40,79 @@ function transferToMaindeck(){
   $("#cardname").val("");
 }
 
-function addCard(text) {
-  var el = document.createElement("a");
-  el.innerHTML = text + "<br/>";
-  getImageUrl(text, function(imgSrc) {
-    $(el).popover({
-      trigger: "hover",
-      html: true,
-      content: '<a class="imgContainer"><img src="' + imgSrc + '" /></a>'
-    });  
-  });
-  $("#cardList").append(el);
-  return el;
+
+
+// function getImageUrl(name,callback) {
+//   return $.getJSON( "https://api.scryfall.com/cards/named?exact=" + name, function( data ) {
+//     callback(data.image_uris.normal);
+//   });
+// }
+
+function getImageUrl(name) {
+  return deckJSON[name].image;
 }
 
-function getImageUrl(name,callback) {
+function getJSONforCard(name, callback) {
   return $.getJSON( "https://api.scryfall.com/cards/named?exact=" + name, function( data ) {
-    callback(data.image_uris.normal);
+    callback(data);
   });
+}
+
+function buildCardListRecursive(list, obj, callback) {
+  c = list.pop();
+  card = c.split('*');
+  getJSONforCard(card[0].trim(), function(data) {
+    obj[data.name] = {
+          'categories': [],
+          'legalities' : data.legalities,
+          'mana_cost' : data.mana_cost,
+          'image' : data.image_uris.normal,
+          'type_lines' : data.type_line
+      };
+      for (i = 1; i < card.length; i++)
+      {
+        obj[data.name].categories.push(card[i].trim());
+      }
+    if (list.length > 0)
+    {
+      buildCardListRecursive(list, obj, callback);
+    }
+    else
+    {
+      callback(obj);
+    }
+  });
+
+}
+
+function buildCardDataFromString(s, callback) {
+  var cardInfoList = s.split('\n');
+  // console.log("l--------------")
+  // console.log(cardInfoList);
+  var finalJSON = {};
+  buildCardListRecursive(cardInfoList, finalJSON, () => {
+    callback(finalJSON);
+  });
+  // get card categories
+  // cardInfoList.forEach(function (c) {
+  //   var card = c.split('*');
+  //   getJSONforCard(card[0].trim(), function(data) {
+  //     finalJSON[data.name] = {
+  //         'categories': [],
+  //         'legalities' : data.legalities,
+  //         'mana_cost' : data.mana_cost,
+  //         // 'image' : data.image_uris.normal,
+  //         'type_lines' : data.type_line
+  //     };
+  //     for (i = 1; i < card.length; i++)
+  //     {
+  //       finalJSON[data.name].categories.push(card[i].trim());
+  //     }
+  //   });
+  // });
+  callback(finalJSON);
+  return finalJSON;
+  // getJSONforCard()
 }
 
 function cardsToObject(s) {
@@ -73,17 +125,120 @@ function cardsToObject(s) {
       if (i != 0)
       {
         tmp.push(card[i].trim());
-      } 
+      }
     }
     dict[card[0].trim()] = tmp;
   });
   return dict;
 }
 
+const VIEWER_CATEGORIES = {
+  custom : {}
+};
+viewer_categories = VIEWER_CATEGORIES;
+
+function addToCategory(name, cat = "Other", num = 1, type = "custom") {
+  // console.log(viewer_categories[type])
+  // for (x in viewer_categories[type])
+    // console.log(x);
+  if (cat in viewer_categories[type])
+  {
+    console.log("Category exists: " + cat);
+    viewer_categories[type][cat].push(name);
+  }
+  else
+  {
+    console.log("Creating category: " + cat);
+    viewer_categories[type][cat] = [name];
+  }
+  //viewer_categories[cat].push
+  // $("#cardList").append("<div id='cat-" + s + "' class='category'><h4>" + s + "</h4></div>");
+}
+
+function categoryExists(s) {
+
+  // return document.getElementById("cat-" + s) != null;
+}
+
 function updateDeck() {
-  $("#cardList")[0].innerHTML = "";
-  var cards = cardsToObject($("#cardEntry").val().trim())
-  for (card in cards) {
-    addCard(card);
+  viewer_categories = VIEWER_CATEGORIES;
+  buildCardDataFromString($("#cardEntry").val(), (json) => {
+    deckJSON = json;
+    for (var key in deckJSON) 
+    {
+      if (deckJSON[key].categories.length == 0)
+        addToCategory(key);
+      else
+        deckJSON[key].categories.forEach(function(cat) {
+          addToCategory(key, cat);
+        });
+    }
+    updatePreview();
+  });
+}
+
+function updatePreview() {
+  var catType = 'custom'
+  for(var cat in viewer_categories[catType])
+  {
+    var catContainer = buildCategoryElement(cat);
+    for(var i in viewer_categories[catType][cat])
+    {
+      console.log(cat + ":" + viewer_categories[catType][cat][i]);
+      buildCardElement(catContainer, viewer_categories[catType][cat][i]);
+    }
   }
 }
+
+function buildCategoryElement(cat) 
+{
+  var el = document.createElement("div");
+  el.setAttribute("id", "cat-" + cat);
+  el.innerHTML = "<h4>" + cat + "</h4>";
+  $("#cardList").append(el);
+  return el;
+}
+
+function buildCardElement(container, name)
+{
+  console.log("building: " + name);
+  var el = document.createElement("a");
+  el.innerHTML = name + "<br/>";
+  var imgSrc = getImageUrl(name);
+  $(el).popover({
+      trigger: "hover",
+      html: true,
+      content: '<a class="imgContainer"><img src="' + imgSrc + '" /></a>'
+    });
+  container.append(el);
+  return el;
+}
+// });
+  // getImageUrl(text, function(imgSrc) {
+  //   $(el).popover({
+  //     trigger: "hover",
+  //     html: true,
+  //     content: '<a class="imgContainer"><img src="' + imgSrc + '" /></a>'
+  //   });
+  // });
+
+//   console.log("object", deckJSON);
+//   // while(!Object.keys(deckJSON).length)
+//   //   console.log(0);
+//   // console.log(deckJSON.Cancel)
+//   $.each($.parseJSON(data), function (key, value) {
+//     alert(value.<propertyname>);
+// });
+  // console.log("t1:", JSON.parse(JSON.stringify(Object.keys(deckJSON))));
+  // console.log("t2:", Object.getOwnPropertyNames(deckJSON));
+  // addCategory("Other");
+  // Object.keys(deckJSON).forEach(function(key) {
+  //   addCard(key);
+  //   // console.log(key)
+  //   // deckJSON[key].categories.forEach(function(cat) {
+  //   //   console.log(cat);
+  //   // })
+  //   // if(!categoryExists())
+
+  // });
+
